fc6ff0d82e384ef1212eb7bbf9466016e811c158
Vincent Berenz <vberenz@tuebingen.mpg.de>
2020-04-08 16:45:49 +0200

    updated doc conf.py to include __init__

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -31,3 +31,10 @@ latex_documents = [
    ur'Nicolas Guetler and Vincent Berenz', 'manual'),
 ]
 
+def skip(app, what, name, obj, would_skip, options):
+    if name == "__init__":
+        return False
+    return would_skip
+
+def setup(app):
+    app.connect("autodoc-skip-member", skip)
